f42d48d95578e0b577eb2f6c269780eb955f4c57
Xubaear <xubaear@gmail.com>
2025-11-10 20:38:10 +0600

    added delete option

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ app.get('/transactions', async (req, res) => {
       res.send(result);
     });
 
+
+    app.delete('/transaction/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await transactionCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
